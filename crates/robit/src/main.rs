fe831d72bb05877e809366bc6783eccd87fcffed
Steven Lee <leexgone@163.com>
2025-08-08 18:04:46 +0800

    feat: 更新依赖项版本并重构主程序以使用新的配置加载方式

--- crates/robit/src/main.rs
+++ crates/robit/src/main.rs
@@ -1,7 +1,9 @@
 use clap::Parser;
 use rig::client::CompletionClient;
 use rig::completion::Prompt;
+use rig::providers::deepseek::ClientBuilder;
 use robit::cli::Cli;
+use robit::config::load_config;
 
 #[tokio::main]
 async fn main() {
@@ -12,12 +14,15 @@ async fn main() {
     //     Err(e) => eprintln!("获取当前工作目录失败: {}", e),
     // }
 
-    let llm = rig::providers::openai::Client::from_url("sk-XXX", "https://api.deepseek.com");
-    let deepseek = llm.agent("deepseek-chat").build();
+    // let llm = rig::providers::openai::Client::from_url("sk-XXX", "https://api.deepseek.com");
+    let config = load_config().unwrap();
+    
+    let llm = ClientBuilder::new(&config.llm().api_key).base_url(&config.llm().base_url).build().unwrap();
+    let agent = llm.agent(&config.llm().model).build();
 
-    let response = deepseek.prompt("你是谁？")
+    let response = agent.prompt("你是谁？")
             .await
-            .expect("Failed to invoke deepseek.");
+            .expect("Failed to invoke llm.");
 
-    println!("DeepSeek: {}", response);
+    println!("{}", response);
 }
